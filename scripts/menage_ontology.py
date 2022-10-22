05b321a5e44487d886f2bb8c3dbf6892733ef7d6
root <serenapaneri2605988@gmail.com>
2022-10-22 15:07:53 +0000

    update

--- scripts/menage_ontology.py
+++ scripts/menage_ontology.py
@@ -1,254 +1,231 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python
 
 import rospy
 import random
+import numpy as np
+from random import randint
 from armor_msgs.srv import *
 from armor_msgs.msg import * 
+from exprob_ass1.srv import Hypothesis
 
-person = []
-weapon = []
-place = []
+people = ['Rev. Green', 'Prof. Plum', 'Col. Mustard','Msr. Peacock', 'Miss. Scarlett', 'Mrs. White']
+weapons = ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Spanner']
+places = ['Conservatory', 'Lounge', 'Kitchen', 'Library', 'Hall', 'Study', 'Ballroom', 'Dining room', 'Billiard room']
+ID = ['0000', '0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009']
 
-
-def load():
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'LOAD'
-    req.primary_command_spec = 'FILE'
-    req.secondary_command_spec = ''
-    req.args = ['/root/ros_ws/src/exprob_ass1/cluedo_ontology.owl', 'http://www.emarolab.it/cluedo-ontology', 'true', 'PELLET', 'true']
-    res = armor_interface(req)   
-    
-
-def wh():
-"""
-  No need since in the ontology we already have the definition who == person ..
-"""
-    # class suspected person
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'QUERY'
-    req.primary_command_spec = 'OBJECTPROP'
-    req.secondary_command_spec = 'IND'
-    req.args = ['who', ID]
-    
-    # class probable implements
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'QUERY'
-    req.primary_command_spec = 'OBJECTPROP'
-    req.secondary_command_spec = 'IND'
-    req.args = ['what', ID]
-    
-    # class suspected scene of murder
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'ADD'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    req.args = ['where', ID]
-
-
-def tbox():
-"""
-  Try to put all the person, weapons and places into an array and then proceed 
-  with a for cycle
-"""
-    global people, weapons, places
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'ADD'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    
-    # instances of the class suspected person
-    people = ['Rev. Green', 'Prof. Plum', 'Col. Mustard','Msr. Peacock', 'Miss. Scarlett', 'Mrs. White']
-    
-    for person in people:
-    	req.args = [person, 'PERSON']
-    	res = armor_interface(req)
-    	print('The suspects have been uploaded in the TBox')
-    
-    # instances of the class probable implements
-    weapons = ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Spanner']
-    for weapon in weapons:
-        req.args = [weapon, 'WEAPON']
-        res = armor_interface(req)
-        print('The implements have been uploaded in the TBox')
-    
-    # instances of the class suspected scenes of murder
-    places = ['Conservatory', 'Lounge', 'Kitchen', 'Library', 'Hall', 'Study', 'Ballroom', 'Dining room', 'Billiard room']
-    for place in places:
-        req.args = [place, 'PLACE']
-        res = armor_interface(req)
-        print('The scenes of murder have been uploaded in the TBox')
-
-    
-def disjoint_classes():
-"""
-  No need since in the ontology the cathegory are already disjoint
-"""
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'DISJOINT'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    req.args = ['PERSON']
-    res = armor_interface(req)
-    req.args = ['WEAPON']
-    res = armor_interface(req)
-    req.args = ['PLACE']
-    res = armor_interface(req)
-    
-
-def reasoner():
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'REASON'
-    req.primary_command_spec = ''
-    req.secondary_command_spec = ''
-    res = armor_interface(req)
-
-
-def complete():
-"""
-  No need since in the ontology a complete hypothesis is already defined
-"""
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'QUERY'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    req.args = ['COMPLETED']
-    res = armor_interface(req)
-
-
-def inconsistent():
-"""
-  No need since in the ontology a complete hypothesis is already defined
-"""
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'QUERY'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    req.args = ['INCONSISTENT']
-    res = armor_interface(req)
-
-
-def save():
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'SAVE'
-    req.primary_command_spec = ''
-    req.secondary_command_spec = ''
-    req.args = ['/root/ros_ws/src/exprob_ass1/final_ontology.owl']
-    res = armor_interface(req)
-
-
-
-
-from exprob_ass1.srv import Hypothesis.srv
-
-hypo = []
-killer = []
-arm = []
-room = []
-ID = None
-
-def all_hypothesis():
-     
-     # inconsistent hypotheses
-     hypo[0].killer.append(random.choice(people))
-     hypo[0].arm.append(random.choice(weapons))
-     hypo[0].arm.append(random.choice(weapons))
-     hypo[0].arm.append(random.choice(weapons))
-     hypo[0].room.append(random.choice(places))
-     hypo[0].ID = 'HPO'
-     
-     hypo[1].room.append(random.choice(places))
-     hypo[1].room.append(random.choice(places))
-     hypo[1].arm.append(random.choice(weapons))
-     hypo[1].arm.append(random.choice(weapons))
-     hypo[1].ID = 'HP1'
-     
-     hypo[2].killer.append(random.choice(people))
-     hypo[2].killer.append(random.choice(people))
-     hypo[2].arm.append(random.choice(weapons))
-     hypo[2].room.append(ramdom.choice(places))
-     hypo[2].ID = 'HP2'
-     
-     # incomplete hypotheses
-     hypo[3].killer.append(random.choice(people))
-     hypo[3].killer.append(random.choice(people))
-     hypo[3].room.append(random.choice(places))
-     hypo[3].ID = 'HP3'
-     
-     hypo[4].room.append(random.choice(places))
-     hypo[4].arm.append(random.choice(weapons))
-     hypo[4].room.append(random.choice(places))
-     hypo[4].ID = 'HP4'
-     
-     hypo[5].killer.append(random.choice(people))
-     hypo[5].arm.append(random.choice(weapons))
-     hypo[5].armo.append(random.choice(weapons))
-     hypo[5].ID = 'HP5'
-     
-     # complete and consistent hypotheses
-     hypo[6].killer.append(random.choice(people))
-     hypo[6].arm.append(random.choice(weapons))
-     hypo[6].room.append(random.choice(places))
-     hypo[6].ID = 'HP6'
-     
-     hypo[7].killer.append(random.choice(people))
-     hypo[7].arm.append(random.choice(weapons))
-     hypo[7].room.append(random.choice(places))
-     hypo[7].ID = 'HP7'
-     
-     hypo[8].killer.append(random.choice(people))
-     hypo[8].arm.append(random.choice(weapons))
-     hypo[8].room.append(random.choice(places))
-     hypo[8].ID = 'HP8'
-     
-     hypo[9].killer.append(random.choice(people))
-     hypo[9].arm.append(random.choice(weapons))
-     hypo[9].room.append(random.choice(places))
-     hypo[9].ID = 'HP9'
-     
-def onto_hypo():
-    req = ArmorDirectiveReq()
-    req.client_name = 'tutorial'
-    req.reference_name = 'ontoTest'
-    req.command = 'ADD'
-    req.primary_command_spec = 'IND'
-    req.secondary_command_spec = 'CLASS'
-    req.args = ['hypothesis', HYPOTHESIS]
-
-
-
-
+hypotheses = []
+feasible_hypotheses = []
+winning_hypothesis = []
 
 def main():
+    global armor_interface
     rospy.init_node('menage_ontology')
     rospy.wait_for_service('armor_interface_srv')
-    armor_iterface = rospy.ServiceProxy('armor_interface_srv', ArmorDirective)
+    print('Waiting for the armor service')
+    armor_interface = rospy.ServiceProxy('armor_interface_srv', ArmorDirective)
+    
+    # loading the cluedo ontology
+    load()
+    # adding the individuals in the TBox
+    tbox()
+    # reason 
+    reasoner()
+    # disjoint the individuals of all the classes
+    disjoint_individuals()
+    # save the new modified ontology
+    
+    all_hypotheses()
+    load_hypotheses()
+    save()
     
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print("fail")
-    
 
+
+def load():
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'LOAD'
+    req.primary_command_spec = 'FILE'
+    req.secondary_command_spec = ''
+    req.args = ['/root/ros_ws/src/exprob_ass1/cluedo_ontology.owl', 'http://www.emarolab.it/cluedo-ontology', 'true', 'PELLET', 'true']
+    msg = armor_interface(req)
+    res = msg.armor_response  
+
+
+def tbox():
+    global people, weapons, places
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'ADD'
+    req.primary_command_spec = 'IND'
+    req.secondary_command_spec = 'CLASS'
+    
+    # instances of the class suspected person
+    for person in people:
+    	req.args = [person, 'PERSON']
+    	msg = armor_interface(req)
+    	res = msg.armor_response
+    print('The suspects have been uploaded in the TBox')
+    
+    # instances of the class probable implements
+    for weapon in weapons:
+        req.args = [weapon, 'WEAPON']
+        msg = armor_interface(req)
+        res = msg.armor_response
+    print('The implements have been uploaded in the TBox')
+    
+    # instances of the class suspected scenes of murder
+    for place in places:
+        req.args = [place, 'PLACE']
+        msg = armor_interface(req)
+        res = msg.armor_response
+    print('The scenes of murder have been uploaded in the TBox')
+
+
+def disjoint_individuals():
+    """
+      This operation need to be done in order to disjoint the individuals
+      from each other 
+    """
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'DISJOINT'
+    req.primary_command_spec = 'IND'
+    req.secondary_command_spec = 'CLASS'
+    req.args = ['PERSON']
+    msg = armor_interface(req)
+    res = msg.armor_response
+    print('The individuals of the class PERSON have been disjoint')
+    req.args = ['WEAPON']
+    msg = armor_interface(req)
+    res = msg.armor_response
+    print('The individuals of the class WEAPON have been disjoint')
+    req.args = ['PLACE']
+    msg = armor_interface(req)
+    res = msg.armor_response
+    print('The individuals of the class PLACE have been disjoint')
+
+
+def reasoner():
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'REASON'
+    req.primary_command_spec = ''
+    req.secondary_command_spec = ''
+    msg = armor_interface(req)
+    res = msg.armor_response
+
+def save():
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'SAVE'
+    req.primary_command_spec = ''
+    req.secondary_command_spec = ''
+    req.args = ['/root/ros_ws/src/exprob_ass1/final_ontology.owl']
+    msg = armor_interface(req)
+    res = msg.armor_response
+    print('The new ontology has been saved under the name final_ontology.owl')
+    
+def search(list_):
+    result = any(item in list_ for item in ID)
+    if result:
+        return True
+    return False
+    
+def list_index(list1, list2):
+      index = [i for i,item in enumerate(list1) if item in list2]
+      return index
+    
+def all_hypotheses():
+
+    hypotheses = [[] for _ in range(10)]
+    
+    # complete and consistent hypotheses
+    hypotheses[0].append(random.choice(people))
+    hypotheses[0].append(random.choice(places))
+    hypotheses[0].append(random.choice(weapons))
+    hypotheses[0].append('0000')
+     
+    hypotheses[1].append(random.choice(people))
+    hypotheses[1].append(random.choice(places))
+    hypotheses[1].append(random.choice(weapons))
+    hypotheses[1].append('0001')
+    
+    hypotheses[2].append(random.choice(people))
+    hypotheses[2].append(random.choice(places))
+    hypotheses[2].append(random.choice(weapons))
+    hypotheses[2].append('0002')
+     
+    hypotheses[3].append(random.choice(people))
+    hypotheses[3].append(random.choice(places))
+    hypotheses[3].append(random.choice(weapons))
+    hypotheses[3].append('0003')
+    
+    # uncomplete hypotheses 
+    hypotheses[4].append(random.choice(people))
+    hypotheses[4].append(random.choice(people))
+    hypotheses[4].append(random.choice(places))
+    hypotheses[4].append('0004')
+    
+    hypotheses[5].append(random.choice(places))
+    hypotheses[5].append(random.choice(places))
+    hypotheses[5].append(random.choice(weapons))
+    hypotheses[5].append('0005')
+    
+    hypotheses[6].append(random.choice(people))
+    hypotheses[6].append(random.choice(weapons))
+    hypotheses[6].append(random.choice(weapons))
+    hypotheses[6].append('0006')
+     
+    # inconsistent hypotheses
+    hypotheses[7].append(random.choice(people))
+    hypotheses[7].append(random.choice(places))
+    hypotheses[7].append(random.choice(weapons))
+    hypotheses[7].append(random.choice(weapons))
+    hypotheses[7].append('0007')
+    
+    hypotheses[8].append(random.choice(people))
+    hypotheses[8].append(random.choice(places))
+    hypotheses[8].append(random.choice(places))
+    hypotheses[8].append(random.choice(weapons))
+    hypotheses[8].append('0008')
+    
+    hypotheses[9].append(random.choice(people))
+    hypotheses[9].append(random.choice(people))
+    hypotheses[9].append(random.choice(people))
+    hypotheses[9].append(random.choice(places))
+    hypotheses[9].append(random.choice(places))
+    hypotheses[9].append(random.choice(weapons))
+    hypotheses[9].append('0009')
+    
+    for i in range(4):
+        feasible_hypotheses.append(hypotheses[i])
+    
+    # select randomly the winning hypothesis
+    n = randint(0, len(feasible_hypotheses))
+    winning_hypothesis.append(feasible_hypotheses[n])
+    
+    return hypotheses, feasible_hypotheses, winning_hypothesis
+    
+def load_hypotheses():
+    req = ArmorDirectiveReq()
+    req.client_name = 'menage_ontology'
+    req.reference_name = 'cluedontology'
+    req.command = 'ADD'
+    req.primary_command_spec = 'IND'
+    req.secondary_command_spec = 'CLASS'
+    req.args = ['hypotheses', 'HYPOTHESIS']
+    res = armor_interface(req)
+    print("All the hypotheses of the game have been uploaded")
+    
 
 
 if __name__ == '__main__':
